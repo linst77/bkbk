558df40480d00fdaac09d099fd975eb319dd584a
linst77 <linst77@gmail.com>
2022-01-14 00:12:22 +0900

    1차 setting update

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!aw9#f9xz#36x^ey5(&&o#+60om)2-$poh)veid-icuc8%(6%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
